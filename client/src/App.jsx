6ade9a3beed2a1d62a0f327d40428c8b06ce8f82
Vitor Augusto <vitoraugreis@gmail.com>
2025-07-20 12:33:22 -0300

    Criação de categoria por meio de API concluida

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
     const [categories, setCategories] = useState([])
+    const [newCategoryName, setNewCategoryName] = useState('')
     useEffect(() => {
         fetch('http://localhost:5255/api/categories')
         .then(res => res.json())
@@ -12,9 +13,41 @@ function App() {
             setCategories(data);
         })
     }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Objeto com os dados que a API espera.
+        const categoryData = {
+            name: newCategoryName
+        };
+
+        fetch('http://localhost:5255/api/categories', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(categoryData),
+        })
+        .then(response => response.json())
+        .then(newlyCreatedCategory => {
+            // Adiciona a nova categoria à lista existente na tela.
+            setCategories(currentCategories => [...currentCategories, newlyCreatedCategory]);
+            setNewCategoryName('');
+        });
+    };
+
     return (
     <div>
         <h1>Categorias</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="Nome da nova categoria"
+                value={newCategoryName}
+                onChange={e => setNewCategoryName(e.target.value)}
+            />
+            <button type="submit">Adicionar</button>
+        </form>
+        <hr/>
         <ul>
         {
             categories.map(category => (
