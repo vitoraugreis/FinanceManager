f69f31f6e48ada3329356e018042b7dbb3ce513b
Vitor Augusto <vitoraugreis@gmail.com>
2025-07-28 19:03:25 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-# Este script inicia o back-end e o front-end, cada um em sua própria janela de terminal.
-
-gnome-terminal --title="Back-end API" -- bash -c "echo 'Iniciando Back-end...'; cd server && dotnet run; exec bash"
-gnome-terminal --title="Front-end Client" -- bash -c "echo 'Iniciando Front-end...'; cd client && npm run dev; exec bash"
